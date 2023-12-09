fc3a184ac4321abfb8044878b1617e7823b806a1
Sergey Letov <letovs87@gmail.com>
2023-12-09 22:45:34 +0300

    Добавлен комент

--- src/Store/Abstract/Context/IDbReader.cs
+++ src/Store/Abstract/Context/IDbReader.cs
@@ -4,6 +4,9 @@ namespace Store.Abstract.Context;
 
 public interface IDbReader
 {
-    
+    /// <summary>
+    /// Чтение базы данных.
+    /// </summary>
+    /// <remarks>.AsNoTracking() - true</remarks>
     IQueryable<TEntity> Read<TEntity>() where TEntity : class, IEntityWithId;
 }
