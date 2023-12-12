9fdd7ba4b04eac8bf313832e6081a4cf942a1d8f
Sergey Letov <letovs87@gmail.com>
2023-12-12 20:03:22 +0300

    Поправилен namespace менеджера миграций

--- src/WebHost/Program.cs
+++ src/WebHost/Program.cs
@@ -1,22 +1,21 @@
-﻿using CustomHost = Microsoft.Extensions.Hosting;
-using Store.DI;
+﻿using Store.DI;
 using Microsoft.Extensions.Hosting;
-using Host;
+using Hosts;
 using Microsoft.Extensions.Configuration;
 using Repositories.DI;
 using Store.ConfigurationOptions;
-using Store.Migrations;
 
 
-CustomHost.Host
+Host
     .CreateDefaultBuilder(args)
     .ConfigureServices((hostBuilder, services) =>
     {
-        var ttt = typeof(ResourcesContextFactory).Assembly.FullName;
-
         var databaseOptions = new DatabaseOptions();
+
         hostBuilder.Configuration.GetSection(nameof(DatabaseOptions)).Bind(databaseOptions);
+
         services.AddStoreDependencies(databaseOptions);
+
         services.AddRepositoriesAndEntityFactory();
     })
     .Build()
