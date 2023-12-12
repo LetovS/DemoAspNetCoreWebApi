c2d9dcffd32dfb217b26bf49d53c79e1939b53c7
Sergey Letov <letovs87@gmail.com>
2023-12-13 00:02:14 +0300

    Зачатки юнит тестов

---
+++ UnitTests/Dependencies/ContainerFixture.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using Program = Web.Host.Program;
+
+namespace UnitTests.Dependencies;
+
+/// <summary>
+/// Fixture контейнера Api
+/// </summary>
+public sealed class ContainerFixture : IDisposable
+{
+    private readonly IHost host;
+
+    /// <summary>
+    /// .ctor
+    /// </summary>
+    public ContainerFixture()
+    {
+        host = Program.CreateHostBuilder(Array.Empty<string>(), b =>
+            {
+                var builder = Program.ConfigureWebHostBuilder(b);
+            })
+            .Build();
+    }
+
+    public IServiceProvider Container => host.Services;
+
+    /// <inheritdoc />
+    public void Dispose()
+    {
+        host.Dispose();
+    }
+}
---
+++ UnitTests/Dependencies/DependenciesTests.cs
@@ -0,0 +1,22 @@
+﻿using Xunit;
+
+namespace UnitTests.Dependencies;
+
+public class DependenciesTests : IClassFixture<ContainerFixture>
+{
+    private readonly IServiceProvider container;
+
+    /// <summary>
+    /// .ctor
+    /// </summary>
+    public DependenciesTests(ContainerFixture fixture)
+    {
+        container = fixture.Container;
+    }
+
+    [Fact]
+    public void Test()
+    {
+        Assert.True(true);
+    }
+}
