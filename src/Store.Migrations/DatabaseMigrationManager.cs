e7b039ae0dea576b4ef53414d8c5ac8444d80ec1
Sergey Letov <letovs87@gmail.com>
2023-12-13 00:03:02 +0300

    Прочее

---
+++ src/Store.Migrations/DatabaseMigrationManager.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+
+namespace Store.Migrations;
+
+public static class DatabaseMigrationManager
+{
+    public static async Task MigrateSchema()
+    {
+        Console.WriteLine("Applying database schema migration...");
+
+        await Migrate<ResourcesContext, ResourcesContextFactory>().ConfigureAwait(false);
+
+        Console.WriteLine("Done");
+    }
+
+    private static async Task Migrate<TDbContext, TDbContextFactory>()
+        where TDbContext : DbContext
+        where TDbContextFactory : IDesignTimeDbContextFactory<TDbContext>, new()
+    {
+        var dbContextFactory = new TDbContextFactory();
+        await using var dbContext = dbContextFactory.CreateDbContext(Array.Empty<string>());
+
+        await dbContext.Database.MigrateAsync().ConfigureAwait(false);
+    }
+}
---
+++ src/WebHost/Startup.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Repositories.DI;
+using Store.ConfigurationOptions;
+using Store.DI;
+
+namespace Web.Host;
+
+public sealed class Startup
+{
+    private readonly IConfiguration configuration;
+
+    public Startup(IConfiguration configuration)
+    {
+        this.configuration = configuration;
+    }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        var databaseOptions = new DatabaseOptions();
+        
+        configuration.GetSection(nameof(DatabaseOptions)).Bind(databaseOptions);
+        
+        services.AddStoreDependencies(databaseOptions);
+
+        services.AddRepositoriesAndEntityFactory();
+    }
+
+    public void Configure(IApplicationBuilder app)
+    {
+        
+    }
+}
+
