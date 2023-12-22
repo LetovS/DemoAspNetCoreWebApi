a457078da1e5c64b7f9a499c2b4bbfc1a82d6790
Sergey Letov <letovs@inbox.ru>
2023-12-22 22:21:06 +0300

    unit test automapper profiles

---
+++ UnitTests/Automapper/ApiAutoMapperTests.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using Business.Automapper;
+using Xunit;
+
+namespace UnitTests.Automapper;
+
+/// <summary>
+/// Тест коинсистенции профайлов
+/// </summary>
+public class ApiAutoMapperTests
+{
+    private readonly IMapper mapper;
+    
+    /// <summary>
+    /// ctor.
+    /// </summary>
+    public ApiAutoMapperTests()
+    {
+        mapper = new MapperConfiguration(p =>
+        {
+            var assembly = new[] { typeof(BusinessProfile).Assembly };
+
+            var profiles = assembly.Select(x => x.GetTypes()
+                    .Where(x => typeof(Profile).IsAssignableFrom(x)))
+                .SelectMany(x => x);
+
+            foreach (var profile in profiles)
+            {
+                p.AddProfile(Activator.CreateInstance(profile) as Profile);
+            }
+        }).CreateMapper();
+    }
+    
+    [Fact(DisplayName = "Проверка валидности маппинга")]
+    public void ShouldBeValid()
+    {
+        mapper.ConfigurationProvider.AssertConfigurationIsValid();
+    }
+}
