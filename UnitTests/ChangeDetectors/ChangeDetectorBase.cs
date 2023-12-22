c2de0837084b0d370f7182ecaf09afc2a84d5c12
Sergey Letov <letovs@inbox.ru>
2023-12-22 23:33:13 +0300

    Добавлены тесты детекторов бизнес изменений

---
+++ UnitTests/ChangeDetectors/ChangeDetectorBase.cs
@@ -0,0 +1,15 @@
+﻿using AutoFixture;
+using AutoFixture.AutoMoq;
+
+namespace UnitTests.ChangeDetectors;
+
+/// <summary>
+/// Базовый детектор изменений
+/// </summary>
+public class ChangeDetectorBase
+{
+    /// <summary>
+    /// Fixture
+    /// </summary>
+    protected readonly IFixture Fixture = new Fixture().Customize(new AutoMoqCustomization());
+}
---
+++ UnitTests/ChangeDetectors/OrderItemChangeDetectorTests.cs
@@ -0,0 +1,66 @@
+﻿using AutoFixture;
+using Business.Abstract.Services;
+using Business.Implementations.HasNoChange;
+using Business.Models.OrderItem;
+using FluentAssertions;
+using Moq;
+using Xunit;
+
+namespace UnitTests.ChangeDetectors;
+
+/// <summary>
+/// Тест детектора изменений товаров
+/// </summary>
+public class OrderItemChangeDetectorTests : ChangeDetectorBase
+{
+    private readonly OrderItemChangeDetector _sut;
+    private readonly Mock<IOrderItemService> _providerReadRepositoryMock;
+
+    /// <summary>
+    /// ctor.
+    /// </summary>
+    public OrderItemChangeDetectorTests()
+    {
+        _providerReadRepositoryMock = Fixture.Freeze<Mock<IOrderItemService>>();
+        _sut = Fixture.Create<OrderItemChangeDetector>();
+    }
+
+    [Fact(DisplayName = "Проверка эквивалентности сущности и модели. Сущность и модель эквивалентны.")]
+    public void HasNoChanges_NoChanges_ShouldBeTrue()
+    {
+        // Arrange
+        var entity = FactoryTestData.CreateNewOrderItem();
+
+        var model = new UpdateOrderItemModel(entity.Name!, entity.Quantity, entity.Unit!, entity.OrderId);
+
+        _providerReadRepositoryMock.Setup(x => x.GetByIdAsync(entity.Id, It.IsAny<CancellationToken>()))
+            .ReturnsAsync(entity);
+
+        // Act
+        var result = _sut.HasNoChanges(entity, model);
+
+        // Assert
+        result.Should().BeTrue();
+    }
+
+    [Fact(DisplayName = "Проверка эквивалентности сущности и модели. Сущность и модель неэквивалентны.")]
+    public void HasNoChanges_WithChanges_ShouldBeFalse()
+    {
+        // Arrange
+        var entity = FactoryTestData.CreateNewOrderItem();
+
+        var model = new UpdateOrderItemModel(Fixture.Create<string>(),
+            Fixture.Create<decimal>(),
+            Fixture.Create<string>(),
+            Fixture.Create<Guid>());
+
+        _providerReadRepositoryMock.Setup(x => x.GetByIdAsync(entity.Id, It.IsAny<CancellationToken>()))
+            .ReturnsAsync(entity);
+
+        // Act
+        var result = _sut.HasNoChanges(entity, model);
+
+        // Assert
+        result.Should().BeFalse();
+    }
+}
