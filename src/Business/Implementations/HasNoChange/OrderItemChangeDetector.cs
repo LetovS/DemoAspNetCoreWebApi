e2c3c05d6ef626447871a85eef6b17b594eefd55
Sergey Letov <letovs@inbox.ru>
2023-12-22 22:16:21 +0300

    ренейм

--- src/Business/Implementations/HasNoChange/OrderItemChangeDetector.cs
+++ src/Business/Implementations/HasNoChange/OrderItemChangeDetector.cs
@@ -7,7 +7,7 @@ namespace Business.Implementations.HasNoChange;
 /// <summary>
 /// Проверяет изменения в товаре
 /// </summary>
-public sealed class OrderItemHasNoChange : IChangeDetector<OrderItemRecord, UpdateOrderItemModel>
+public sealed class OrderItemChangeDetector : IChangeDetector<OrderItemRecord, UpdateOrderItemModel>
 {
     /// <inheritdoc/>
     public bool HasNoChanges(OrderItemRecord entity, UpdateOrderItemModel model)
