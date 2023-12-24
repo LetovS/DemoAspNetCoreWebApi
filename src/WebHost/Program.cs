baae9c8d20534290ec66acb5aca17faf06c03766
Sergey Letov <letovs@inbox.ru>
2023-12-24 22:19:35 +0300

    Запуск веб апи перенесен в веб хост, тесты зависимостей поломались

--- src/WebHost/Program.cs
+++ src/WebHost/Program.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Hosting;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 using Store.Migrations;
 using System.Runtime.CompilerServices;
 
@@ -7,15 +8,35 @@ namespace Web.Hosts;
 
 internal static class Program
 {
-    internal static void Main(string[] args)
+    internal static async Task Main(string[] args)
     {
         DatabaseMigrationManager.MigrateSchema().ConfigureAwait(false);
-        CreateHostBuilder(args).Build().Run();
+        //CreateHostBuilder(args).Build().Run();
+
+
+        await CreateHostBuilder(args, b => ConfigureWebHostBuilder(b))
+            .Build()
+            .RunAsync();
     }
-    internal static IHostBuilder CreateHostBuilder(string[] args) =>
-                                    Host.CreateDefaultBuilder(args).ConfigureServices((x,y) =>
-                                    {
-                                        var config = x.Configuration;
-                                        y.ConfigureServices(config);
-                                    });
+    //internal static IHostBuilder CreateHostBuilder(string[] args) =>
+    //                                Host.CreateDefaultBuilder(args).ConfigureServices((x,y) =>
+    //                                {
+    //                                    var config = x.Configuration;
+    //                                    y.ConfigureServices(config);
+    //                                });
+
+    /// <summary>
+    ///  Создает построитель хоста
+    /// </summary>
+    public static IHostBuilder CreateHostBuilder(string[] args, Action<IWebHostBuilder> webHostBuilderConfigurator)
+        => Host
+            .CreateDefaultBuilder()
+            .ConfigureWebHostDefaults(webHostBuilderConfigurator);
+
+    /// <summary>
+    /// Создает построитель веб хост
+    /// </summary>
+    public static IWebHostBuilder ConfigureWebHostBuilder(IWebHostBuilder webHostBuilder)
+        => webHostBuilder
+            .UseStartup<Startup>();
 }
--- src/WebHost/Startup.cs
+++ src/WebHost/Startup.cs
@@ -1,22 +1,50 @@
 ﻿using Business.DI;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using Repositories.DI;
 using Store.ConfigurationOptions;
 using Store.DI;
+using System.Reflection;
 
 namespace Web.Hosts;
 
 /// <summary>
 /// Конфигурация хоста <see cref="Program"/>
 /// </summary>
-public static class Startup
+public class Startup
 {
+    private readonly IConfiguration configuration;
+
+    /// <summary>
+    /// ctor.
+    /// </summary>
+    public Startup(IConfiguration configuration)
+    {
+        this.configuration = configuration;
+    }
     /// <summary>
     /// Конфигуратор сервисов
     /// </summary>
-    public static IServiceCollection ConfigureServices(this IServiceCollection services, IConfiguration configuration)
+    public void ConfigureServices(IServiceCollection services)
     {
+        services.AddControllers();
+        services.AddEndpointsApiExplorer();
+
+        services.AddSwaggerGen(options =>
+        {
+            options.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Title = "Web API",
+                Version = "v1"
+            });
+            var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+            options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+        });
+
         var databaseOptions = new DatabaseOptions();
         
         configuration.GetSection(nameof(DatabaseOptions)).Bind(databaseOptions);
@@ -25,9 +53,38 @@ public static class Startup
 
         services.AddRepositoriesAndEntityFactory();
 
-        services.AddBusinessServices();
-        
-        return services;
-    }   
+        services.AddBusinessServices();        
+    }
+
+    /// <summary>
+    /// Настройка ПО промежуточного слоя
+    /// </summary>
+    /// <param name="app"></param>
+    /// <param name="env"></param>
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (!env.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+            app.UseDeveloperExceptionPage();
+        }
+
+        //app.UseAuthorization();
+
+        //app.MapControllers();
+
+        app.UseHttpsRedirection();
+
+        app.UseStaticFiles();
+
+        //app.UseCookiePolicy();
+
+        //app.UseAuthentication();
+                
+        app.UseRouting();
+
+        app.UseEndpoints(options => options.MapControllers());
+    }
 }
 
