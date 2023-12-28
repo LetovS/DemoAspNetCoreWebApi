a788a63962d823ab359e9c839bb2dd5b13578584
Sergey Letov <letovs@inbox.ru>
2023-12-28 19:18:02 +0300

    Починил пространства имен

---
+++ src/Web.Host/Startup.cs
@@ -0,0 +1,33 @@
+﻿using Business.DI;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Repositories.DI;
+using Store.ConfigurationOptions;
+using Store.DI;
+
+namespace Web.Hosts;
+
+/// <summary>
+/// Конфигурация хоста <see cref="Program"/>
+/// </summary>
+public static class Startup
+{
+    /// <summary>
+    /// Конфигуратор сервисов
+    /// </summary>
+    public static IServiceCollection ConfigureServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        var databaseOptions = new DatabaseOptions();
+        
+        configuration.GetSection(nameof(DatabaseOptions)).Bind(databaseOptions);
+        
+        services.AddStoreDependencies(databaseOptions);
+
+        services.AddRepositoriesAndEntityFactory();
+
+        services.AddBusinessServices();
+        
+        return services;
+    }   
+}
+
