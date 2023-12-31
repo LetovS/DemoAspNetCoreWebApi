53e9d68e86ad84f706e61a97f343aed0698b4df2
Sergey Letov <letovs@inbox.ru>
2023-12-31 23:08:48 +0300

    Реализован маппинг request -> busoness models

--- src/Web.Api/Automapper/WebApiMapperProfile.cs
+++ src/Web.Api/Automapper/WebApiMapperProfile.cs
@@ -1,8 +1,12 @@
 ﻿using AutoMapper;
+using Business.Models.Order;
+using Business.Models.OrderItem;
 using Business.Models.Provider;
+using Microsoft.Data.SqlClient;
 using Store.Entities;
 using Web.Api.Automapper.Converters;
 using Web.Contracts.Models.Order;
+using Web.Contracts.Models.OrderItem;
 using Web.Contracts.Models.Provider;
 
 namespace Web.Api.Automapper;
@@ -17,6 +21,7 @@ public class WebApiMapperProfile : Profile
     /// </summary>
     public WebApiMapperProfile()
     {
+        #region Маппинг провайдера
         CreateMap<CreateProviderRequest, CreateProviderModel>()            
             .ForCtorParam("providerName", x => x.MapFrom(y => y.Name));
 
@@ -27,7 +32,37 @@ public class WebApiMapperProfile : Profile
             .ForCtorParam("id", opt => opt.MapFrom(y => y.Id))
             .ForCtorParam("name", opt => opt.MapFrom(y => y.ProviderName))
             .ForMember(x => x.Orders, opt => opt.Ignore());
-            
+
+        #endregion
+
+        #region Маппинг заказа
+        CreateMap<CreateOrderRequest, CreateOrderModel>()
+            .ForCtorParam("orderNumber", opt => opt.MapFrom(x => x.OrderNumber))
+            .ForCtorParam("orderDate", opt => opt.MapFrom(y => y.OrderDate))
+            .ForCtorParam("providerId", opt => opt.MapFrom(y => y.ProviderId));
+        
+        CreateMap<UpdateOrderRequest, UpdateOrderModel>()
+            .ForCtorParam("orderNumber", opt => opt.MapFrom(x => x.OrderNumber))
+            .ForCtorParam("orderDate", opt => opt.MapFrom(y => y.OrderDate))
+            .ForCtorParam("providerId", opt => opt.MapFrom(y => y.ProviderId));
+
+        CreateMap<OrderRecord, OrderResponse>()
+            .ForCtorParam("id", opt => opt.MapFrom(y => y.Id))
+            .ForCtorParam("orderNumber", opt => opt.MapFrom(x => x.OrderNumber))
+            .ForCtorParam("orderDate", opt => opt.MapFrom(y => y.OrderDate))
+            .ForCtorParam("providerId", opt => opt.MapFrom(y => y.ProviderId))
+            .ForMember(x => x.OrderItems, opt => opt.Ignore());
+        #endregion
+
+        #region Маппинг элементов
+
+        CreateMap<CreateOrderItemRequest, CreateOrderItemModel>();
+
+        CreateMap<UpdateOrderItemRequest, UpdateOrderItemModel>();
+        
+        CreateMap<OrderItemRecord, OrderItemResponse>();
+        
+        #endregion
     }   
 }
 
