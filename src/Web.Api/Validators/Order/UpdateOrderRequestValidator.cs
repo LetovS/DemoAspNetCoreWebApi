97e9016962e33ff74eb368b8bc23fe7e87c3f09a
Sergey Letov <letovs@inbox.ru>
2024-01-02 17:29:05 +0300

    Валидация запросов

---
+++ src/Web.Api/Validators/Order/UpdateOrderRequestValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using Web.Contracts.Models.Order;
+
+namespace Web.Api.Validators.Order;
+
+/// <summary>
+/// Валидация обновления заказа
+/// </summary>
+public class UpdateOrderRequestValidator : AbstractValidator<UpdateOrderRequest>
+{
+    /// <summary>
+    /// ctor.
+    /// </summary>
+    public UpdateOrderRequestValidator()
+    {
+        
+    }
+}
